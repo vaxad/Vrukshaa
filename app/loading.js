e770dbd777414a975b4f868062b9615807047932
vaxad <varadprabhu111@gmail.com>
2023-12-06 22:05:16 +0530

    hiding sensitive links

--- app/loading.js
+++ app/loading.js
@@ -4,10 +4,10 @@ import { Jelly } from '@uiball/loaders'
 export default function loading() {
   return (
     <div className=' flex w-full justify-center py-48 items-center'>
-                <Jelly
-                            size={80}
-                            speed={0.9}
-                            color="white"
-                        /></div>
+      <Jelly
+        size={80}
+        speed={0.9}
+        color="white"
+      /></div>
   )
 }
